fac9c49f72fde9e6727a27c798299a27e4968796
Wenderson José <wenderson.teles@icloud.com>
2026-02-15 11:38:05 -0300

    refactor: melhorias dos componentes do mapa e marcadores temporários

--- src/features/map/components/TemporaryMarker.jsx
+++ src/features/map/components/TemporaryMarker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { Marker } from 'react-leaflet';
 
 const TemporaryMarker = ({ position, children }) => {
